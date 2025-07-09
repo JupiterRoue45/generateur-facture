d4bcc805b7b2b88592a7152ff67228d4352b7e20
JupiterRoue45 <idrisbenarbia1@gmail.com>
2025-07-09 21:07:45 +0200

    derniere retouches

--- gestion/forms.py
+++ gestion/forms.py
@@ -1,6 +1,8 @@
 from django import forms
 from .models import Produit
 
+# class pour ajouter une ligne de facture (un produit avec sa quantité) lors de la génération d'une facture
+
 class LigneFactureForm(forms.Form):
     produit = forms.ModelChoiceField(queryset=Produit.objects.all(), label="Produit")
     quantite = forms.IntegerField(min_value=1, label="Quantité")
--- gestion/views.py
+++ gestion/views.py
@@ -1,6 +1,5 @@
 from django import forms
 from django.shortcuts import redirect, render
-from django.http import HttpResponse
 from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 from .forms import LigneFactureForm
@@ -8,10 +7,14 @@ from .models import Facture, LigneFacture, Produit
 from django.shortcuts import get_object_or_404
 from django.core.paginator import Paginator
 
+# class pour affichage des listes
+
 class ProduitListView(ListView):
     model = Produit
     template_name = 'produit_liste.html'
     paginate_by = 5  
+
+# class pour création et l'ajout d'un produit
 
 class ProduitCreateView(CreateView):
     model = Produit
@@ -19,16 +22,22 @@ class ProduitCreateView(CreateView):
     template_name = 'produit_form.html'
     success_url = reverse_lazy('produit_liste')
 
+# class pour la modification d'un produit
+
 class ProduitUpdateView(UpdateView):
     model = Produit
     fields = ['nom', 'prix', 'date_peremption']
     template_name = 'produit_form.html'
     success_url = reverse_lazy('produit_liste')
 
+# class pour la suppression d'un produit
+
 class ProduitDeleteView(DeleteView):
     model = Produit
     template_name = 'produit_confirm_delete.html'
     success_url = reverse_lazy('produit_liste')
+
+# Méthode pour créer une facture
 
 def creer_facture(request):
     LigneFormSet = forms.formset_factory(LigneFactureForm, extra=1)  # 1 lignes par défaut
@@ -61,6 +70,8 @@ def facture_detail(request, pk):
         'lignes': lignes
     })
 
+# Méthode pour afficher la liste des factures
+
 def facture_liste(request):
     factures = Facture.objects.order_by('-date_creation')
     paginator = Paginator(factures, 5)  # 5 factures par page
@@ -72,6 +83,7 @@ def facture_liste(request):
         'page_obj': page_obj
     })
 
+# Méthode pour afficher les détails d'une facture
 
 def facture_detail(request, pk):
     facture = get_object_or_404(Facture, pk=pk)
